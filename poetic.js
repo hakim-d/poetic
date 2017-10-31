d1a4543f4de1dd9cdeb8fc9742e6cb5d81402c26
Hakim Dahoune <hakim.dahoune@gmail.com>
2017-10-31 00:08:12 +0000

    first commit

---
+++ poetic.js
@@ -0,0 +1,47 @@
+/*
+ * PoeticJS
+ * The classical Arabic poems are written such that each verse is written as two halves on the same row and separated by one or more punctuation marks or spaces. This Javascript library can do that easily.
+ * Copyright (C) 2017 Hakim DAHOUNE (http://hakim.ma, @7akim_d)
+ */
+
+var poetic = (function (){
+
+  function columnize ( target = "body", color = "#000", delimiter = "*", delimiter_color = null, delimiter_size = "100%", line_height = "normal" ) {
+    // Create the poem styles
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    if (!delimiter_color) delimiter_color = color;
+    style.innerHTML = `.poetic {color:${color}; direction: rtl; font-size: 1.6vw;}
+                        @media only screen and (max-width: 789px) {.poetic{font-size: 2.6vw;}}
+                        .poetic sadr, .poetic ajuz {
+                         width: 44%; text-align: justify; text-align-last: justify; display: inline-block; font-size: 100%;
+                        }
+                        .poetic bayt {line-height: ${line_height}; display: block;}
+                        .poetic sadr {padding-left: 6%;}
+                        .poetic ajuz {padding-right: 6%;}
+                        .poetic bayt::after {content: "${delimiter}";font-size: ${delimiter_size}; color: ${delimiter_color};position: absolute;right: 50%; margin-right: -10px;width: 20px;height: 20px;text-align:center;}
+                       `;
+    document.getElementsByTagName('head')[0].appendChild(style);
+
+    // Define the DOM target to apply the poem formatting
+    var bind = document.querySelector(target);
+    bind.className += " poetic";
+    var text = bind.innerHTML;
+    text = "\n" + text.trim();
+    text = text.replace(/\n/gi, function checkStr(x,y){
+      if ( y == 0 ) return "<bayt><sadr>" + x;
+      else return "</ajuz></bayt>" + x + "<bayt><sadr>";
+    });
+    text = text.replace(/#/gi, function checkStr(x,y){
+      return "</sadr><ajuz>";
+    });
+
+    // Apply the poem formatting
+    bind.innerHTML = text;
+  }
+  
+  return {
+    columnize: columnize
+  };
+  
+})();
